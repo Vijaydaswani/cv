9a51983197ab7492d294e0acf3dccc42245dba7f
Vijay Daswani <vijay.daswani@gmail.com>
2024-10-15 03:58:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loader = document.getElementById('loader');
     const content = document.querySelector('.content');
-    const sections = document.querySelectorAll('section');
-
+    
     // Hide loader and show content
     setTimeout(() => {
         loader.style.display = 'none';
         content.style.opacity = 1;
-
-        // Add in-view class to sections
-        sections.forEach(section => {
-            section.classList.add('in-view');
-        });
     }, 2000);
 });
