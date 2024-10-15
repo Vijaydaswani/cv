db313323aef1d1907591041a9cfceac4db399208
Vijay Daswani <vijay.daswani@gmail.com>
2024-10-15 04:03:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('section');
+
     const options = {
         root: null,
-        threshold: 0.1,
+        rootMargin: '0px',
+        threshold: 0.1
     };
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.add('fade-in-visible');
+                entry.target.classList.add('fade-in');
             }
         });
     }, options);
